cef60d9b267c37c884157b552b07da4ed429cdda
hyuk <k941026h@naver.com>
2024-12-31 11:09:37 +0900

    challenge project start

---
+++ src/test/java/com/bb/challenge/FifteenDollarChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bb.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifteenDollarChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
